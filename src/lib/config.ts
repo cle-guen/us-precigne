0245f2ea5846c4fbb7a272cebf6357f24a92215c
Cedrick <monsieur.cedrick@gmail.com>
2025-09-22 14:00:45 +0200

    og

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
 // Configuration de l'application
 export const config = {
-  baseUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https://us-precigne.vercel.app',
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
 } as const
